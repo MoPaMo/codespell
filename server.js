139b6f741260d2b0cb9d2fbbfbb46fb551e9440f
MoPaMo <mopamo@users.noreply.github.com>
2024-12-08 10:43:08 +0100

    reinstate path i apparently accidentally delted

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const UPLOADS_DIR = path.join(__dirname, "uploads");
 if (!fs.existsSync(UPLOADS_DIR)) {
   fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 }
+// Home Page - Upload Form
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Set EJS as templating engine
 app.set("view engine", "ejs");
@@ -142,7 +146,7 @@ app.post("/upload", async (req, res) => {
 
 // Redirect all 404s to homepage
 app.use((req, res, next) => {
-  res.status(404).redirect("/");
+  if (req.path != "/") res.status(404).redirect("/");
 });
 // Start the Server!!
 app.listen(PORT, () => {
